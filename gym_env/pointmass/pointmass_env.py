b7630b93401c9b5a0e520681375c82c01fc28ae3
chuck <you@example.com>
2020-10-21 16:09:00 -0400

    new cost function, and reset

--- gym_env/pointmass/pointmass_env.py
+++ gym_env/pointmass/pointmass_env.py
@@ -30,8 +30,11 @@ class PointMassEnv(gym.Env):
         self.landmark.orientation = None
 
     def _reset_agent(self):
-        self.agent.position = 0.1 * self.xlim * np.random.uniform(0, 1, self.dim) - self.xlim
-        self.landmark.position = self.xlim - 0.1 * self.xlim * np.random.uniform(0, 1, self.dim)
+        # self.agent.position = 0.1 * self.xlim * np.random.uniform(0, 1, self.dim) - self.xlim
+        # self.landmark.position = self.xlim - 0.1 * self.xlim * np.random.uniform(0, 1, self.dim)
+
+        self.agent.position = 0.1 * self.xlim * np.array([1,1]) - self.xlim
+        self.landmark.position = - self.agent.position
 
     def seed(self, seed=None):
         random.seed(seed)
@@ -62,8 +65,38 @@ class PointMassEnv(gym.Env):
 
         reward = -np.linalg.norm(self.agent.position - self.landmark.position)
 
-        cost = 0 if np.linalg.norm(self.agent.position) > self.radius and \
-            np.max(np.abs(self.agent.position)) < self.xlim else 1
+        obstacle_type = 1   # choose from 1~6
+        if obstacle_type == 1: # one circle, binary cost
+            cost = 0 if np.linalg.norm(self.agent.position) > self.radius and \
+                np.max(np.abs(self.agent.position)) < self.xlim else 1
+        elif obstacle_type == 2: # one smaller square, binary cost
+            xlim_smaller = .5 * self.xlim
+            cost = 0 if np.max(np.abs(self.agent.position)) > xlim_smaller and \
+                        np.max(np.abs(self.agent.position)) < self.xlim else 1
+        elif obstacle_type == 3: #two circles, binary cost
+            center_1 = np.array([-.5, .5])
+            center_2 = -center_1
+            cost = 0 if np.linalg.norm(self.agent.position - center_1) > self.radius and \
+                        np.linalg.norm(self.agent.position - center_2) > self.radius and \
+                        np.max(np.abs(self.agent.position)) < self.xlim else 1
+        elif obstacle_type == 4:  # one circle, continuous cost
+            dist_to_center = np.linalg.norm(self.agent.position)
+            cost = 0 if dist_to_center > self.radius and \
+                        np.max(np.abs(self.agent.position)) < self.xlim else 2 * np.exp(-dist_to_center) + .5
+        elif obstacle_type == 5:  # one smaller square, continuous cost
+            dist_to_center = np.linalg.norm(self.agent.position)
+            xlim_smaller = .5 * self.xlim
+            cost = 0 if np.max(np.abs(self.agent.position)) > xlim_smaller and \
+                        np.max(np.abs(self.agent.position)) < self.xlim else 2 * np.exp(-dist_to_center) + .5
+        elif obstacle_type == 6:  # two circles, continuous cost
+            dist_to_center = np.linalg.norm(self.agent.position)
+            center_1 = np.array([-.5, .5])
+            center_2 = -center_1
+            cost = 0 if np.linalg.norm(self.agent.position - center_1) > self.radius and \
+                        np.linalg.norm(self.agent.position - center_2) > self.radius and \
+                        np.max(np.abs(self.agent.position)) < self.xlim else 2 * np.exp(-dist_to_center) + .5
+        else:
+            raise ValueError
         info = {}
         info['cost'] = cost
 
